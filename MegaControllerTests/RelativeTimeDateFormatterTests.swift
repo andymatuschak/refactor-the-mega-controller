9e4c2025026d8f1b885b5a66208389c7575fb1f3
anhdat <mr.anhdat@gmail.com>
2015-12-10 00:10:37 +0700

    Add unittests for `getNumberOfCalendarDaysBetweenDates`

--- MegaControllerTests/RelativeTimeDateFormatterTests.swift
+++ MegaControllerTests/RelativeTimeDateFormatterTests.swift
@@ -9,6 +9,50 @@
 @testable import MegaController
 import XCTest
 
+
+class GetNumberOfCalendarDaysBetweenDatesTests: XCTestCase {
+    let calendar = NSCalendar.currentCalendar()
+    let baseDate = NSDate(timeIntervalSinceReferenceDate: 0)
+    
+    var dateFormatter: RelativeTimeDateFormatter!
+    override func setUp() {
+        dateFormatter = RelativeTimeDateFormatter(calendar: calendar)
+    }
+    
+    func testRelativeToToday() {
+        let testDate = calendar.dateByAddingUnit(.Hour, value: 1, toDate: baseDate, options: NSCalendarOptions())!
+        XCTAssertEqual(
+            dateFormatter.getNumberOfCalendarDaysBetweenDates(baseDate, testDate),
+            0
+        )
+    }
+    
+    func testRelativeToTomorrow() {
+        let testDate = calendar.dateByAddingUnit(.Day, value: 1, toDate: baseDate, options: NSCalendarOptions())!
+        XCTAssertEqual(
+            dateFormatter.getNumberOfCalendarDaysBetweenDates(baseDate, testDate),
+            1
+        )
+    }
+    
+    func testRelativeToLaterDate() {
+        let testDate = calendar.dateByAddingUnit(.Day, value: 4, toDate: baseDate, options: NSCalendarOptions())!
+        XCTAssertEqual(
+            dateFormatter.getNumberOfCalendarDaysBetweenDates(baseDate, testDate),
+            4
+        )
+    }
+    
+    func testRelativeToEarlierDate() {
+        let testDate = calendar.dateByAddingUnit(.Day, value: -4, toDate: baseDate, options: NSCalendarOptions())!
+        XCTAssertEqual(
+            dateFormatter.getNumberOfCalendarDaysBetweenDates(baseDate, testDate),
+            -4
+        )
+    }
+}
+
+
 class RelativeTimeDateFormatterTests: XCTestCase {
     let calendar = NSCalendar.currentCalendar()
     let baseDate = NSDate(timeIntervalSinceReferenceDate: 0)
